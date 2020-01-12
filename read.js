2a9dc6bf1bb7fbdf51d18fd3c161492fb9b7cf73
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 01:52:45 +0530

    read and write sync
    read and write sync

--- read.js
+++ read.js
@@ -4,3 +4,10 @@ var readMe = fs.readFileSync('readMe.txt','utf8');
 console.log(readMe);
 
 // create one more file name it as readMe.txt and write some thing in that.
+
+var fs = require('fs')
+
+var readMe = fs.readFileSync('readMe.txt','utf8');
+fs.writeFileSync('writeme.txt', readMe);
+
+//write file,there are all async type
