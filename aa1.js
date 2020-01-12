0b6c53ae746fdeb7ff82eed925d574425088412f
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 00:14:18 +0530

    aa1 different way
    aa1

---
+++ aa1.js
@@ -0,0 +1,9 @@
+module.exports.counter = function(arr){
+  return 'there are ' + arr.length +  'elements in this array';
+};
+
+module.exports.adder = function(a,b){
+  return `the sum of 2 numbers is ${a+b}`;
+
+};
+module.exports. pi = 3.142;
