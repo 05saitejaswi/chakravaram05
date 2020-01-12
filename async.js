8d4519ee09e4e57bb21e84a65c1412d9a41a6752
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 02:00:07 +0530

    async
    ascyn

---
+++ async.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+fs.readFile('readMe.txt','utf8',function(err,data){
+    console.log(data);
+});
+
+//output: hello ninja
+
+var fs = require('fs');
+
+fs.readFile('readMe.txt','utf8',function(err,data){
+    console.log(data);
+});
+
+console.log('test');
+
+//output:test  hello ninja
