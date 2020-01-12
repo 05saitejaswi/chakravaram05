0637985d6f39f2d58da97b741459d5f6c39ed40e
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 00:21:09 +0530

    stuff.js
    stuff.js

---
+++ stuff.js
@@ -0,0 +1,11 @@
+var stuff = require('./aaa');
+
+console.log(stuff.counter(['sham','ram','gopal']));
+console.log(stuff.adder(5,6));
+console.log(stuf.adder(stuff.pi,5));
+
+
+//output is 
+//there are 3elements in this array
+//the sum of 2 numbers is 11
+//the sum of 2 numbers is 8.142
