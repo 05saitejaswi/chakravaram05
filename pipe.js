504347a814404ad9d2cb60c047810a3464dce2ae
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 13:16:29 +0530

    pipe.js
    pipe

---
+++ pipe.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var fs = require('fs');
+
+
+var server = http.createServer(function(req,res){
+    console.log('request was made:' +req.url);
+   res.writeHead(200, {'content-Type':'text/plain'});
+   var myReadstream = fs.createReadStream(__dirname + '/readMe.txt','utf8');
+   //var myWritestream = fs.createWriteStream(__dirname +'/writeme.txt');
+   myReadstream.pipe(res);
+
+});
+
+
+server.listen(3000,'127.0.0.1');
+console.log('yo dawgs,now lising to 3000');
+
+
