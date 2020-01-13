50108a5f1ec10a17775ecdd22a73f63fa82b1d2d
chakravaram.saitejaswi <50818486+05saitejaswi@users.noreply.github.com>
2020-01-13 11:37:24 +0530

    chunk
    chunk

---
+++ chunk.js
@@ -0,0 +1,10 @@
+var http = require('http');
+var fs = require('fs');
+
+
+var myReadstream = fs.createReadStream(__dirname + '/readMe.txt');
+
+myReadstream.on('data',function(chunk){
+    console.log('new chunk received:');
+    console.log(chunk);
+});
